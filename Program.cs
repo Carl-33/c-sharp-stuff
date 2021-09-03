b09e58ccc147657f08fc40f28aad54b8e413f844
carl-33 <koepkecj@gmail.com>
2021-09-03 15:38:39 -0500

    added another variable for fun

--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ namespace c_stuff
         {
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            var lastName = Console.ReadLine();
             var currentDate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.WriteLine($"{Environment.NewLine}Hello, {name} {lastName}, it is {currentDate:t} on {currentDate:d}!");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
         }
